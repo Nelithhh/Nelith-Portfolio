1e9de27780dd03210aaf90e610ef5600fa261ce5
Nelith Karunarathne <nelith550@gmail.com>
2024-07-04 16:35:55 +0530

    Created components
    Home.jsx
    Home.css
    Social.jsx
    Data.jsx

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Social() {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/nelithhhh/" className="home__social-icon" target='_blank'>
+            <i class="uil uil-instagram"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/nelith-karunarathne-a32b5a186/" className="home__social-icon" target='_blank'>
+            <i class="uil uil-linkedin-alt"></i>
+        </a>
+
+        <a href="https://github.com/Nelithhh" className="home__social-icon" target='_blank'>
+            <i class="uil uil-github-alt"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
