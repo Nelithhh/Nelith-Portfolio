c028e2d4dcf12f86fba0d8ff467776ca1d78daa8
Nelith Karunarathne <nelith550@gmail.com>
2024-07-05 18:46:22 +0530

    Qualificati9on component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Services from './components/services/Services';
+import Qualifications from './components/qualifications/Qualifications';
 
 function App() {
 
@@ -17,6 +18,7 @@ function App() {
         <About />
         <Skills />
         <Services />
+        <Qualifications />
       </main>
     </>
   )
