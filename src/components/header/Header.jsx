537229f8d85c853f18957e62aedeb4bf175d52bd
Nelith Karunarathne <nelith550@gmail.com>
2024-07-10 01:25:36 +0530

    Added favicon image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
   return (
     <header className="header">
         <nav className="nav container">
-            <a href="index.html" className="nav__logo">Nelith</a>
+            <a href="index.html" className="nav__logo">Nelith Karunarathne</a>
 
             <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
                 <ul className="nav__list grid">
