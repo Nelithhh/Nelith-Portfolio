794d646f003e930f515c6a3b819f505cbcecf8a2
Nelith Karunarathne <nelith550@gmail.com>
2024-07-11 02:20:17 +0530

    Portfolio

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -1,26 +1,34 @@
-import Image1 from "../../assets/work1.jpg";
-import Image2 from "../../assets/work2.jpg";
-import Image3 from "../../assets/work3.jpg";
+import Image1 from "../../assets/harmony.jpg";
+import Image2 from "../../assets/invento.jpg";
+import Image3 from "../../assets/hathi.jpg";
+import Image4 from "../../assets/hostel.jpg";
 
 export const Data = [
     {
         id: 1,
         image: Image1,
-        title: "Jhon",
-        description: "Description about work"
+        title: "Harmony Fitness",
+        description: "A workout app which is created using React.js and Tailwind CSS that can generate workouts according to your prefference."
     },
 
     {
         id: 2,
         image: Image2,
-        title: "Jhon",
-        description: "Description about work"
+        title: "INVENTO",
+        description: "An JAVA based inventory management system which can track your inventory."
     },
 
     {
         id: 3,
         image: Image3,
-        title: "Jhon",
-        description: "Description about work"
+        title: "Hathi App",
+        description: "An mobile application created to find and sell services online which is created using DART and flutter."
+    },
+
+    {
+        id: 4,
+        image: Image4,
+        title: "Green Supermarket",
+        description: "An e-commerce website which is created to sell products online and intergrated with E-mail APIs and payment gateways."
     }
 ];
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import "./Portfolio.css";
 import { Data } from "./Data";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
 
 function Portfolio() {
   return (
@@ -8,18 +17,36 @@ function Portfolio() {
         <h2 className="section__title">Portfolio</h2>
         <span className="section__subtitle">My projects</span>
 
-        <div className="portfolio__container">
+        <Swiper 
+            className="portfolio__container"
+            loop={true}
+            grabCursor={true}
+            spaceBetween={24}
+            pagination={{
+            clickable: true,
+            }}
+            breakpoints={{
+            576: {
+                slidesPerView: 2,
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 48,
+            },
+            }}
+            modules={[Pagination]}
+        >
             {Data.map(({ id, image, title, description }) => {
                 return (
-                    <div className="portfolio__card" key={id}>
+                    <SwiperSlide className="portfolio__card" key={id}>
                         <img src={image} alt="" className="portfolio__img" />
 
                         <h3 className="portfolio__name">{title}</h3>
                         <p className="portfolio__description">{description}</p>
-                    </div>
+                    </SwiperSlide>
                 );
             })}
-        </div>
+        </Swiper>
     </section>
   )
 }
